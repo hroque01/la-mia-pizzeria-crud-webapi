8201262b917aa25c2c8fa36da07ee992a40ec53b
Hadson Roque <nineonezero.light@gmail.com>
2023-05-15 11:11:26 +0200

    API GetPizzaId

--- la-mia-pizzeria-static/Controllers/PizzasController.cs
+++ la-mia-pizzeria-static/Controllers/PizzasController.cs
@@ -1,6 +1,9 @@
 ﻿using la_mia_pizzeria_static.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace la_mia_pizzeria_static.Controllers
 {
@@ -35,6 +38,59 @@ namespace la_mia_pizzeria_static.Controllers
                 //return la pizza inserita nella [STRING]
                 return Ok(pizze);
             }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> GetPizzaId(int id)
+        {
+            using (PizzaContext ctx = new PizzaContext())
+            {
+                var options = new JsonSerializerOptions
+                {
+                    ReferenceHandler = ReferenceHandler.Preserve
+                };
+                var pizza = ctx.Pizza.Where(p => p.Id == id)
+                                     .Include(p => p.Category)
+                                     .Include(p => p.Ingredients)
+                                     .FirstOrDefault();
+
+                if (pizza == null)
+                    return NotFound();
+
+                var json = JsonSerializer.Serialize(pizza, options);
+                return Content(json, "application/json");
+            }
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Create(Pizza data)
+        {
+            using (PizzaContext ctx = new PizzaContext())
+            {
+                Pizza pizza = new Pizza();
+                pizza.Nome = data.Nome;
+                pizza.Descrizione = data.Descrizione;
+                pizza.Prezzo = data.Prezzo;
+                pizza.Img = data.Img;
+                pizza.CategoryId = data.CategoryId;
+
+                if (data.Ingredients != null)
+                {
+                    foreach (Ingredients ingredient in data.Ingredients)
+                    {
+                        int ingredientId = ingredient.Id;
+                        Ingredients qualcosa = ctx.Ingredients.Where(ing => ing.Id == ingredientId).FirstOrDefault();
+                        pizza.Ingredients.Add(qualcosa);
+                    }
+                }
+
+                ctx.Add(pizza);
+                ctx.SaveChanges();
+
+                return Ok();
+            }            
         }
     }
 }
